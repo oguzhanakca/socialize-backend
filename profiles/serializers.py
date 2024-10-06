675acfb2b3bb0729035083bdd5a3c49707172835
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-06 15:46:00 +0200

    user authentication added for logged in user

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,16 +7,21 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     image = serializers.ImageField(write_only=True)
     image_url = serializers.CharField(source='image.url', read_only=True)
+    is_owner = serializers.SerializerMethodField()
     
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+        
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name', 'bio', 'image', 'image_url'
+            'id', 'owner', 'created_at', 'updated_at', 'name', 'bio',
+            'image', 'image_url', 'is_owner'
         ]
         
     def create(self, validated_data):
         image = validated_data.pop('image')
-        # Cloudinary'ye dosyayı yüklüyoruz
         upload_data = cloudinary.uploader.upload(image)
         validated_data['image'] = upload_data['public_id']
         return Profile.objects.create(**validated_data)
