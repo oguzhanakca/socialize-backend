61d8007def5933e80be47206dac5cd6030cf5766
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-06 18:11:40 +0200

    post tests made, errors fixed by adding default field in post model

--- posts/models.py
+++ posts/models.py
@@ -29,7 +29,7 @@ class Post(models.Model):
         'post_image', default= 'default_post_uu0i5n'
     )
     image_filter = models.CharField(
-        max_length=32, choices=image_filter_choices
+        max_length=32, choices=image_filter_choices, default='normal'
     )
 
     class Meta:
