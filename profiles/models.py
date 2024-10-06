160ec3fb1534a7abf383a6fc5a0399c06e640955
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-06 03:37:27 +0200

    cloudinary upload problem solved, profile image field changed with Cloudinaryfield

--- profiles/models.py
+++ profiles/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.db.models.signals import post_save
+from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
+    name = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(
-        upload_to='images/', default='../default_profile_wxoxmn'
+    image = CloudinaryField(
+        'profile_picture', default= 'default_profile_wxoxmn'
     )
     
     class Meta:
@@ -16,3 +19,10 @@ class Profile(models.Model):
         
     def __str__(self):
         return f"{self.owner}'s profile"
+    
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+post_save.connect(create_profile, sender=User)
--- socialize_backend/settings.py
+++ socialize_backend/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'cloudinary',
     
     'profiles',
+    
 ]
 
 MIDDLEWARE = [
