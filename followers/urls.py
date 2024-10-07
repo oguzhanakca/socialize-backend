39b46803a0a11c01a6cda918e8e9cd281500a684
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-07 23:19:38 +0200

    followers app and follower model created

---
+++ followers/urls.py
@@ -0,0 +1,10 @@
+# from django.urls import path
+# from likes import views
+
+# urlpatterns = [
+#     path('postlikes/', views.PostLikeList.as_view()),
+#     path('postlikes/<int:pk>', views.PostLikeDetail.as_view()),
+#     path('commentlikes/', views.CommentLikeList.as_view()),
+#     path('commentlikes/<int:pk>', views.CommentLikeDetail.as_view()),
+    
+# ]
