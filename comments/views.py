a11daf2605af909d1ad8893bf4e6ca19e58d0f4a
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-09 23:17:22 +0200

    django filter added to profiles and comments

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from socialize_backend.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentDetailSerializer, CommentSerializer
@@ -12,7 +13,11 @@ class CommentList(generics.ListCreateAPIView):
         commentlikes_count=Count('comment_likes', distinct=True)
     ).order_by('-created_at')
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        DjangoFilterBackend
+    ]
+    filterset_fields = [
+        'post'
     ]
     ordering_filters = [
         'commentlikes_count',
