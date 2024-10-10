b66160a48bac68775a7a1c2776dd26be52e8fe08
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-10 17:12:46 +0200

    allowedhosts fixed one more time

--- socialize_backend/settings.py
+++ socialize_backend/settings.py
@@ -65,7 +65,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'socialize-backend-app.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'https://socialize-backend-app-9beed826b5f2.herokuapp.com/']
 
 
 # Application definition
