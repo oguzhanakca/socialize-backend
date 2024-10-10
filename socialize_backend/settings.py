857e28b9d8333dfc8ca8d62b3b007fe4955a5628
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-10 22:46:42 +0200

    client origin commented

--- socialize_backend/settings.py
+++ socialize_backend/settings.py
@@ -115,14 +115,14 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
-    ]
+# if 'CLIENT_ORIGIN' in os.environ:
+#     CORS_ALLOWED_ORIGINS = [
+#         os.environ.get('CLIENT_ORIGIN')
+#     ]
+# else:
+#     CORS_ALLOWED_ORIGIN_REGEXES = [
+#         r"^https://.*\.gitpod\.io$",
+#     ]
     
 CORS_ALLOW_CREDENTIALS = True
 
