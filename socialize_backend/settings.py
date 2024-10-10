411a28274696ff92d154989df512a9052eb210dd
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-10 22:45:36 +0200

    origin regex fixed

--- socialize_backend/settings.py
+++ socialize_backend/settings.py
@@ -121,7 +121,7 @@ if 'CLIENT_ORIGIN' in os.environ:
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        "localhost"
+        r"^https://.*\.gitpod\.io$",
     ]
     
 CORS_ALLOW_CREDENTIALS = True
