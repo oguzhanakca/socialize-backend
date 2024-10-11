b215d86036e6dbb5cf4c4150a9ff239365bd9812
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-11 04:36:13 +0200

    jwt settings changed

--- socialize_backend/settings.py
+++ socialize_backend/settings.py
@@ -44,13 +44,11 @@ if 'DEV' not in os.environ:
         'rest_framework.renderers.JSONRenderer',
     ]
 
-REST_AUTH = {
-    'USE_JWT': True,
-    'JWT_AUTH_COOKIE': 'my-app-auth',
-    'JWT_AUTH_REFRESH_COOKIE': 'my-refresh-token',
-    'JWT_AUTH_SECURE': True,
-    'JWT_AUTH_SAMESITE': 'None',
-}
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH = {
     'USER_DETAILS_SERIALIZER': 'socialize_backend.serializers.CurrentUserSerializer'
