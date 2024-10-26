6d887914770c0d33fa1ea5c5e14d9fa0518f5bba
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-26 18:13:30 +0200

    profile views updated after private profile feature

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.db import models
 from django.db.models import Count
 from rest_framework import generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
@@ -10,11 +11,6 @@ class ProfileList(generics.ListAPIView):
     """
     List all profiles
     """
-    queryset = Profile.objects.annotate(
-        posts_count=Count('owner__post', distinct=True),
-        followers_count=Count('owner__followed', distinct=True),
-        following_count=Count('owner__following', distinct=True)
-    ).order_by('-created_at')
     serializer_class = ProfileSerializer
     filter_backends = [
         filters.OrderingFilter,
@@ -28,6 +24,13 @@ class ProfileList(generics.ListAPIView):
         'posts_count', 'followers_count', 'following_count',
         'owner__following__created_at', 'owner__followed__created_at'
     ]
+    def get_queryset(self):
+        user = self.request.user
+        
+        return Profile.objects.annotate(
+            posts_count=Count('owner__post', distinct=True),followers_count=Count('owner__followed', distinct=True),following_count=Count('owner__following', distinct=True)
+            ).filter(models.Q(is_private=False) | models.Q(owner__following__followed=user)).order_by('-created_at')
+    
     
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
@@ -36,8 +39,10 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.annotate(
+    def get_queryset(self):
+        user = self.request.user
+        return Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
         followers_count=Count('owner__followed', distinct=True),
         following_count=Count('owner__following', distinct=True)
-    ).order_by('-created_at')
+    ).filter(models.Q(is_private=False) | models.Q(owner__following__followed=user)).order_by('-created_at')
