63c375cb35f9240353ffb142fe4d43097719a41a
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-26 22:15:30 +0200

    permissions reverted

--- socialize_backend/permissions.py
+++ socialize_backend/permissions.py
@@ -5,14 +5,14 @@ from followers.models import Follower
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
     If profile is private, only profile owner and followers can see posts.
-    If not, everyone can ready posts and profile owner can CRUD.
+    If not, everyone can read posts and profile owner can CRUD.
     """
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             if not obj.owner.profile.is_private:  
                 return True
             return Follower.objects.filter(
-                owner=request.owner, followed=obj.user
+                owner=obj.owner, followed=request.user
             ).exists() or obj.owner == request.user
         return obj.owner == request.user
     
