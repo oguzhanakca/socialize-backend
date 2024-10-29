78c13fe99775e7fc753fede8e5e2f089d7c14a0c
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-29 23:13:34 +0100

    settings typo fixed

--- socialize_backend/settings.py
+++ socialize_backend/settings.py
@@ -45,7 +45,7 @@ if 'DEV' not in os.environ:
     ]
     
 REST_AUTH = {
-    'USE_JWT': True,
+    'REST_USE_JWT': True,
     'JWT_AUTH_SECURE': True,
     'JWT_AUTH_COOKIE': 'my-app-auth',
     'JWT_AUTH_REFRESH_COOKIE': 'my-refresh-token',
