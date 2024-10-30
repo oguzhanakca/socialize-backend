a9c921027b2054dc2fc7686afb2b646a7ebf066b
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-30 01:22:54 +0100

    urls changed

--- socialize_backend/urls.py
+++ socialize_backend/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import logout_route
-from django.views.generic import TemplateView
+from .views import root_route, logout_route
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', root_route),
     path('admin/', admin.site.urls),
     path('api/api-auth/', include('rest_framework.urls')),
     path('api/dj-rest-auth/logout/', logout_route),
@@ -33,6 +32,4 @@ urlpatterns = [
     path('api/', include('followers.urls')),
     path('api/', include('messaging.urls')),
 ]
-
-handler404 = TemplateView.as_view(template_name='index.html')
 
