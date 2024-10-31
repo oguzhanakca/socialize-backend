11db89293ab6994c43038a6893a5f4f74b4ed62a
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-31 16:53:09 +0100

    api route changed

--- socialize_backend/urls.py
+++ socialize_backend/urls.py
@@ -19,17 +19,17 @@ from django.urls import path, include
 from .views import root_route, logout_route
 
 urlpatterns = [
-    path('api/', root_route),
-    path('api/admin/', admin.site.urls),
-    path('api/api-auth/', include('rest_framework.urls')),
-    path('api/dj-rest-auth/logout/', logout_route),
-    path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('api/dj-rest-auth/registration/', include(
+    path('', root_route),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/logout/', logout_route),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include(
         'dj_rest_auth.registration.urls')),
-    path('api/', include('profiles.urls')),
-    path('api/', include('posts.urls')),
-    path('api/', include('comments.urls')),
-    path('api/', include('likes.urls')),
-    path('api/', include('followers.urls')),
-    path('api/', include('messaging.urls')),
+    path('', include('profiles.urls')),
+    path('', include('posts.urls')),
+    path('', include('comments.urls')),
+    path('', include('likes.urls')),
+    path('', include('followers.urls')),
+    path('', include('messaging.urls')),
 ]
