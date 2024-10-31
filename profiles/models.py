5804fe630bf0f1991181503cf33077e59abd00a0
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-31 15:39:24 +0100

    name field removed from profiles

--- profiles/models.py
+++ profiles/models.py
@@ -11,7 +11,6 @@ class Profile(models.Model):
     """
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
-    name = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     image = CloudinaryField(
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -33,7 +33,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name', 'bio',
+            'id', 'owner', 'created_at', 'updated_at', 'bio',
             'is_private', 'image', 'image_url', 'is_owner',
             'following_id', 'posts_count', 'followers_count', 'following_count'
         ]
